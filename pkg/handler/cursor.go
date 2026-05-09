e642a9d893fffb79266a80e8e9f33bc62ac184af
duststars <i@exploro.one>
2026-05-09 12:04:00 +0800

    Remove cursor refreshing logs

--- pkg/handler/cursor.go
+++ pkg/handler/cursor.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"context"
-	"log"
 	"time"
 
 	"github.com/google/uuid"
@@ -30,24 +29,19 @@ func (cursorObj *Cursor[T]) GetId() string {
 
 // Run() blocks until the end of the cursor's lifecycle: either expired or upstream closed.
 func (cursorObj *Cursor[T]) Run(upstreamChan <-chan T) {
-	cId := cursorObj.cursorId
 	cLife := cursorObj.lifeSpan
-	log.Printf("cursor %s clean timer is set", cId)
 	defer close(cursorObj.ProducerChan)
 	for {
 		select {
 		case <-time.After(cLife):
-			log.Printf("cursor %s expired due to timeout", cId)
 			return
 		case val, ok := <-upstreamChan:
 			if !ok {
 				// upstream is closed, so the cursor have to be close as well.
-				log.Printf("cursor %s closing due to upstream closed", cId)
 				return
 			}
 			cursorObj.ProducerChan <- val
 			// activity took place, so, refresh the expiry
-			log.Printf("cursor %s expiry refresh due to value consuming activity", cId)
 			continue
 		}
 	}
