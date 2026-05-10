2e13e66f3a30cd4d9db7239f39516a01011f49e0
duststars <i@exploro.one>
2026-05-10 10:16:26 +0800

    Updated auxiliary data ingestion script

--- docker/prod/scripts/ingest-data.sh
+++ docker/prod/scripts/ingest-data.sh
@@ -11,6 +11,8 @@ DATA_DIR=${DATA_DIR:-$(realpath "${script_dir}/../../..")}
 
 printf 'DATA_DIR=%s\n' "$DATA_DIR"
 
+mkdir -p "$DATA_DIR"
+
 run_ingest() {
   docker run \
     --network pgsql-prod \
@@ -29,6 +31,22 @@ run_ingest() {
       "$@"
 }
 
+echo "Downloading master4_latest.mrt.bz2..."
+curl -k -L -o "${DATA_DIR}/master4_latest.mrt.bz2" --proxy http://192.168.250.2:1081 \
+    https://mrt.collector.dn42/master4_latest.mrt.bz2
+
+echo "Ingesting master4_latest.mrt.bz2..."
 run_ingest --provider dn42-master4 --sink postgres /app/data/master4_latest.mrt.bz2
+
+echo "Downloading master6_latest.mrt.bz2..."
+curl -k -L -o "${DATA_DIR}/master6_latest.mrt.bz2" --proxy http://192.168.250.2:1081 \
+    https://mrt.collector.dn42/master6_latest.mrt.bz2
+
+echo "Ingesting master6_latest.mrt.bz2..."
 run_ingest --provider dn42-master6 --sink postgres /app/data/master6_latest.mrt.bz2
+
+echo "Downloading latest-bview.gz..."
+curl -o "${DATA_DIR}/latest-bview.gz" -L https://data.ris.ripe.net/rrc00/latest-bview.gz
+
+echo "Ingesting latest-bview.gz..."
 run_ingest --provider ripe-ris-rrc0 --sink postgres /app/data/latest-bview.gz
